fb8783d35e3da7c6923f00c119f3e202cd14da00
= <=>
2022-05-02 10:35:51 +0530

    changed intro text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -35,13 +35,13 @@ export default function() {
                 </ul>
                 <h1 class="typing-reveal">TITLE/NAME</h1>
                 <hr />
-                <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, labore aliquam?</blockquote>
+                <blockquote>Normality is a paved road: It's comfortable to walk, but no flowers grow</blockquote>
                 <cite>- Vincent Van Gogh</cite>
                 <hr />
             </section>
 
             <section class="intro">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eius modi assumenda, consequatur incidunt asperiores quas exercitationem ut voluptates repellat voluptas magnam perferendis cum cumque nesciunt laboriosam atque vitae accusantium.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ducimus distinctio, in ullam deleniti dolorem itaque nulla. Voluptatem iste ut beatae nostrum iusto quaerat voluptates ea eos, eligendi, similique vitae porro voluptas repellat. Odit maiores recusandae soluta tempore repellat! Necessitatibus minus unde delectus? Harum quas neque voluptatem architecto incidunt cum voluptas doloremque libero velit provident aliquam debitis commodi tenetur quos ipsa sed sit inventore temporibus maxime porro esse, odit accusantium molestias enim. Ratione repellat quaerat cumque praesentium! Itaque amet qui animi omnis eos. Magnam, hic officia. Doloremque a vero eaque? Commodi rem libero optio architecto, quia cum repellendus consequuntur adipisci cumque aut consequatur quod minima fuga accusamus velit assumenda blanditiis laboriosam vitae eaque tempora harum itaque quasi. Excepturi fugiat explicabo quos cupiditate dicta, ipsa enim nemo quam earum autem sapiente neque assumenda laudantium reprehenderit repellendus. Amet laboriosam sint natus deserunt repellendus esse harum ea quis fugit veniam expedita debitis ullam qui modi, commodi explicabo atque libero? Qui nemo numquam rem laborum vitae voluptatum doloremque, deleniti fugiat dicta culpa, odio necessitatibus, placeat similique aspernatur cumque quas eos nesciunt magni! Asperiores ad, omnis voluptate unde dolor deleniti consequatur itaque. Ratione natus laudantium dignissimos autem voluptatem magni, magnam commodi placeat odio non optio!</p>
             </section>
 
             <PostsSection className='blog' title="Our Recent Posts">
